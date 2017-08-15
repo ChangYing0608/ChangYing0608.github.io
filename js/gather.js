b3a7610884b008e139cb057c9406e8c1951d6f8d
2256671067@qq.com <2256671067@qq.com>
2017-08-15 14:25:54 +0800

    Site updated: 2017-08-15 14:25:53

---
+++ js/gather.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ccb780e6a96614f4f58495433601af53";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
